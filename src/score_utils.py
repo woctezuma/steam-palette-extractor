32cc50442f0e6d5079f33f977e504643d3fa7d8f
Wok <woctezuma@users.noreply.github.com>
2024-01-07 12:18:59 +0100

    Fix TypeError: unsupported operand type(s) for |: 'builtin_function_or_method' and 'type'

--- src/score_utils.py
+++ src/score_utils.py
@@ -5,20 +5,20 @@ def to_score(
     minimal_distances: torch.tensor,
     indices: torch.tensor,
     params: dict,
-) -> torch.tensor | float:
+) -> torch.tensor:
     factor = params["factor"]
     exponent = params["exponent"]
 
     rank_weights = (1 + factor * indices) ** exponent
     score = minimal_distances * rank_weights
-    return score.sum(dim=1) if len(score.size()) > 1 else score.sum().item()
+    return score.sum(dim=1) if len(score.size()) > 1 else score.sum()
 
 
 def compute_distance_between_palettes(
     v: torch.tensor,
     w: torch.tensor,
     params: dict,
-) -> torch.tensor | float:
+) -> torch.tensor:
     pairwise_distances = torch.cdist(
         v.float(),
         w.float(),
