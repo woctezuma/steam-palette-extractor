f7042bad3f10fa667b65b5002412c1d80dcfebc6
Wok <woctezuma@users.noreply.github.com>
2024-01-07 10:11:35 +0100

    Vectorize the computation of the distance between the reference colors and the palettes

--- src/score_utils.py
+++ src/score_utils.py
@@ -5,19 +5,20 @@ def to_score(
     minimal_distances: torch.tensor,
     indices: torch.tensor,
     params: dict,
-) -> float:
+) -> torch.tensor | float:
     factor = params["factor"]
     exponent = params["exponent"]
 
     rank_weights = (1 + factor * indices) ** exponent
-    return (minimal_distances * rank_weights).sum().item()
+    score = minimal_distances * rank_weights
+    return score.sum(dim=1) if len(score.size()) > 1 else score.sum().item()
 
 
 def compute_distance_between_palettes(
     v: torch.tensor,
     w: torch.tensor,
     params: dict,
-) -> float:
+) -> torch.tensor | float:
     pairwise_distances = torch.cdist(
         v.float(),
         w.float(),
@@ -25,13 +26,13 @@ def compute_distance_between_palettes(
 
     # The first score
     minimal_distances, indices = pairwise_distances.min(
-        dim=0,  # caveat
+        dim=len(pairwise_distances.size()) - 2,
     )
     score_for_w = to_score(minimal_distances, indices, params)
 
     # The second score, in order to make the distance symmetrical
     minimal_distances, indices = pairwise_distances.min(
-        dim=1,  # caveat
+        dim=len(pairwise_distances.size()) - 1,
     )
     score_for_v = to_score(minimal_distances, indices, params)
 
