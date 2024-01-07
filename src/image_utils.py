27dbac4c19b9c588e8b3eba49cff05c581147713
Wok <woctezuma@users.noreply.github.com>
2024-01-07 12:15:48 +0100

    Improve consistency: treat a single image as a batch of size 1 when calling to_linear_hsv()

--- src/image_utils.py
+++ src/image_utils.py
@@ -8,10 +8,9 @@ from src.display_utils import show_colors
 
 def prepare_image(path_or_url: str, params: dict, verbose: bool = True) -> torch.tensor:
     dominant_colors = extract_colors(path_or_url, num_colors=NUM_COLORS)
+    reference_colors = torch.tensor(dominant_colors).unsqueeze(dim=0)
     if params["use_hsv"]:
-        reference_colors = to_linear_hsv(dominant_colors, params["change_coordinates"])
-    else:
-        reference_colors = torch.tensor(dominant_colors)
+        reference_colors = to_linear_hsv(reference_colors, params["change_coordinates"])
 
     if verbose:
         media.show_image(media.read_image(path_or_url), width=616)
