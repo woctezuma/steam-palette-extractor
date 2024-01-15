f529ec4e90d3cacd97d676679a1f68f97f530e89
Wok <woctezuma@users.noreply.github.com>
2024-01-15 15:09:19 +0100

    Minor refactoring: rename a variable for consistency

--- src/score_utils.py
+++ src/score_utils.py
@@ -3,9 +3,9 @@ import torch
 from src.weight_utils import normalize_weights, to_weights
 
 
-def to_weights_source(indices_source, params, num_columns):
+def to_weights_source(indices_source, params, num_elements):
     # The following normalized values lie between 0 and 1.
-    normalized_indices_source = indices_source / (num_columns - 1)
+    normalized_indices_source = indices_source / (num_elements - 1)
     rank_weights_source = to_weights(
         normalized_indices_source,
         params["factor_source"],
@@ -13,11 +13,11 @@ def to_weights_source(indices_source, params, num_columns):
     return normalize_weights(rank_weights_source)
 
 
-def to_weights_delta(indices_target, indices_source, params, num_columns):
+def to_weights_delta(indices_target, indices_source, params, num_elements):
     delta_indices = indices_target - indices_source
 
     # The following normalized values lie between -1 and 1.
-    normalized_delta_indices = delta_indices / (num_columns - 1)
+    normalized_delta_indices = delta_indices / (num_elements - 1)
 
     # Threshold, to ensure that there is no penalty when the matched color has
     # a lower index in the target palette than the color in the source palette.
