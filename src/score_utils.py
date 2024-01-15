5fd434db7ed0ab73f4cc5097f2f6d45f52b6d138
Wok <woctezuma@users.noreply.github.com>
2024-01-15 14:59:05 +0100

    Add a function argument dim to normalize_weights()

--- src/score_utils.py
+++ src/score_utils.py
@@ -1,8 +1,8 @@
 import torch
 
 
-def normalize_weights(weights):
-    return weights / torch.linalg.vector_norm(weights, ord=0, dim=-1, keepdim=True)
+def normalize_weights(weights, dim=-1):
+    return weights / torch.linalg.vector_norm(weights, ord=0, dim=dim, keepdim=True)
 
 
 def to_weights(indices, factor):
