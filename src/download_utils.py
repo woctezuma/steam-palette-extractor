02fdbd108dcdf41da3069859770154967bc39e28
Wok <woctezuma@users.noreply.github.com>
2024-12-07 19:38:28 +0100

    Specify encoding in open()

--- src/download_utils.py
+++ src/download_utils.py
@@ -12,7 +12,7 @@ def write_to_text_file(
     fname: str,
     img_name: str = IMG_NAME,
 ) -> None:
-    with Path(fname).open("w") as f:
+    with Path(fname).open("w", encoding="utf-8") as f:
         for app_id in app_ids:
             url = get_image_url(app_id, img_name)
             f.write(f"{url}\n")
